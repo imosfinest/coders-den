04d69611a16484d13563e0b161f831d0ab2b9863
OPARA GOODNESS <68869066+imosfinest@users.noreply.github.com>
2024-03-16 11:02:38 +0400

    Add files via upload

---
+++ day1/datatypes.js
@@ -0,0 +1,7 @@
+// 5. Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+console.log(typeof "Goodness"); // string
+console.log(typeof false); // boolean
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
+
+// DONE
---
+++ day1/variable.js
@@ -0,0 +1,53 @@
+//  4. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+let myName = "Goodness"; // string assigned
+
+let sheIsAboy = false; // boolean assigned
+
+let lastName; // undefined assigned
+
+let value = null; // null assigned
+
+// 6. Four variables without assigning values
+let a;
+
+let car;
+
+let town;
+
+let home;
+
+// 7. Declare four variables with assigned values
+let b = 4;
+
+let weight = 75; // in kilogram
+
+let address = "closeBy";
+
+let people = "too many";
+
+// 8. Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let firstName = "Goodness";
+
+let last_Name = "Goody";
+
+let maritalStatus = "single";
+
+let country = "UAE";
+
+let age = 33;
+
+// 9. Declare variables to store your first name, last name, marital status, country and age in a single line
+let First_name = "goodness",
+  Last_name = "opara",
+  Marital_status = "Single",
+  Country = "uae",
+  Age = "1year";
+
+//10.  Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+let myAge = 10;
+console.log(myAge); // 10
+
+let yourAge = 11;
+console.log(yourAge); //11
+
+// DONE
